4a435a646b1519c7d39908c04a18a1539037ee41
lukblan <luk.blanko@gmail.com>
2023-12-13 09:48:40 -0300

    Add basic project folders and classes

---
+++ src/services/file_reader.rb
@@ -0,0 +1,7 @@
+class FileReader
+  def read_file(file_name)
+    File.foreach("./../../examples/#{file_name}") do |line|
+      puts line
+    end
+  end
+end
---
+++ src/ui/console_interface.rb
@@ -0,0 +1,3 @@
+class ConsoleInterface
+
+end
