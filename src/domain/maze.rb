2255ce28c25dc287f7af06d35c37877be0329651
lukblan <luk.blanko@gmail.com>
2023-12-14 10:27:52 -0300

    Add logic to solve maze

--- src/domain/maze.rb
+++ src/domain/maze.rb
@@ -5,8 +5,50 @@ class Maze
     @end_point = end_point
   end
 
+  def solve
+    solve_maze(@start_position)
+    @map_grid
+  end
+
+  def solve_maze(position)
+    row = position[0]
+    column = position[1]
+
+    if [row, column] == @end_point
+      return
+    end
+    self.mark_grid(row, column)
+
+    if can_move(row - 1, column)
+      solve_maze([row - 1, column])
+    elsif can_move(row, column + 1)
+      solve_maze([row, column + 1])
+    elsif can_move(row + 1, column)
+      solve_maze([row + 1, column])
+    elsif can_move(row, column - 1)
+      solve_maze([row, column - 1])
+    end
+  end
+
+  def can_move(row, column)
+    row_max_index = @map_grid.length - 1
+    column_max_index = @map_grid[0].length - 1
+    (row >= 0 && row <= row_max_index) &&
+      (column >= 0 && column <= column_max_index) && @map_grid[row][column] == self.white_space
+
+  end
+
+  def mark_grid(row, column)
+    if @map_grid[row][column] == self.white_space
+      @map_grid[row][column] = self.mark
+    end
+  end
 
   def mark
     "x"
+  end
+
+  def white_space
+    " "
   end
 end
--- src/ui/console_interface.rb
+++ src/ui/console_interface.rb
@@ -9,11 +9,22 @@ class ConsoleInterface
     file_name = self.get_file_name
     @maze = maze_factory.build(file_reader, file_name)
     solved_maze = @maze.solve
+    print_maze(solved_maze)
   end
 
   def get_file_name
     print("File name: ")
     file_name = gets.chomp
     "./../maze-files/#{file_name}.txt"
+  end
+
+  def print_maze(solved_maze)
+    solved_maze.each do |row|
+      row.each do |column|
+        print column
+      end
+
+      puts
+    end
   end
 end
